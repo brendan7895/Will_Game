dbdf99d4239b114753e945ef0c3dc367c8fc5a28
brendan7895 <43366422+brendan7895@users.noreply.github.com>
2020-09-30 11:39:41 +0200

    Commit 1

---
+++ WillGame/Assets/Scripts/Caustics.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Caustics : MonoBehaviour
+{
+    private Projector projector;
+    public Texture2D[] frames;
+    private float fps = 25;
+    private int frameIndex;
+
+    //// Start is called before the first frame update
+    void Start()
+    {
+        projector = GetComponent<Projector>();
+
+        NextFrame();
+        InvokeRepeating("NextFrame", 1 / fps, 1 / fps);
+    }
+
+    void NextFrame()
+    {
+        projector.material.SetTexture("_ShadowTex", frames[frameIndex]);
+        frameIndex++;
+        if (frameIndex >= frames.Length)
+        {
+            frameIndex = 0;
+        }
+    }
+}
---
+++ WillGame/Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    [Range(0, 10)]
+    public int heightScale = 5;
+    [Range(0, 10)]
+    public float detailScale = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x) / detailScale,
+                (vertices[i].z + this.transform.position.z) / detailScale) * heightScale;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+    }   
+}

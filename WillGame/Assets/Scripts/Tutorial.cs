57868979f37cd3bad0282dac2ce054a293122e1b
brendan7895 <43366422+brendan7895@users.noreply.github.com>
2020-10-26 16:07:09 +0200

    Left mouse

--- WillGame/Assets/Scripts/Tutorial.cs
+++ WillGame/Assets/Scripts/Tutorial.cs
@@ -100,7 +100,7 @@ public class Tutorial : MonoBehaviour
 
         if (Vector3.Distance(player.transform.position, shark.transform.position) <= 10)
         {
-            if (Input.GetKeyDown(KeyCode.T))
+            if (Input.GetMouseButtonDown(0))
             {
                 if (movementComplete == true && dodgeComplete == true && interactComplete == true && boostComplete == true)
                 {

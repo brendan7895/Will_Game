0c56fff0dc8227246707953accd4d75f0973dbf7
brendan7895 <43366422+brendan7895@users.noreply.github.com>
2020-11-22 13:35:27 +0200

    Added fish

--- WillGame/Assets/Scripts/PlaceCoral.cs
+++ WillGame/Assets/Scripts/PlaceCoral.cs
@@ -6,15 +6,19 @@ using UnityEngine;
 public class PlaceCoral : MonoBehaviour
 {
     public GameObject[] coralTypes = new GameObject[4];
+    public GameObject[] fishType = new GameObject[2];
 
     //for 1
     Transform[] coral;
     public List<GameObject> corals;
     GameObject[] placeCoral = new GameObject[400];
 
-    string json;
+    Transform[] fish;
+    public List<GameObject> fishList;
+    GameObject[] placeFish = new GameObject[200];
 
     public LayerMask ground;
+    Vector3 groundPos;
 
     void Start()
     {
@@ -44,7 +48,7 @@ public class PlaceCoral : MonoBehaviour
             Ray ray = new Ray(placeCoral[i].transform.position, Vector3.down);
             if (Physics.Raycast(ray, out hit, 20, ground))
             {
-                Vector3 groundPos = new Vector3(
+                groundPos = new Vector3(
                     placeCoral[i].transform.position.x,
                     hit.point.y,
                     placeCoral[i].transform.position.z);
@@ -55,13 +59,35 @@ public class PlaceCoral : MonoBehaviour
             
         }
 
-        //for (int i = 0; i < placeCoral.Length; i++)
-        //{
-        //    json += JsonUtility.ToJson(placeCoral[i].transform.position);
-        //    json += "\n";
+        for (int i = 0; i < fishType.Length; i++)
+        {
+
+            fish = fishType[i].GetComponentsInChildren<Transform>();
+            foreach (Transform child in fish)
+            {
+                if (child.gameObject.GetInstanceID() != fishType[i].GetInstanceID())
+                {
+                    fishList.Add(child.gameObject);
+                }
+
+            }
+        }
+
+        for (int i = 0; i < placeFish.Length; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-100, 100), Random.Range(groundPos.y+1, 15), Random.Range(-100, 100));
+            int index = Random.Range(0, fishList.Count);
             
-        //}
-        //Debug.Log(json);
-        //File.WriteAllText(Application.dataPath + "/coral.json", json);
+            placeFish[i] = Instantiate(fishList[index], pos, Quaternion.identity);
+            placeFish[i].transform.Rotate(-90, Random.Range(0, 360), 90);
+
+            if(placeFish[i].tag == "Fish2")
+            {
+                Vector3 scale = new Vector3(0.1f, 0.1f, 0.1f);
+                placeFish[i].transform.localScale = scale;
+            }
+
+        }
+
     }
 }

8b1b7c86e9e0e129b3845bd680a18184b26291ec
brendan7895 <43366422+brendan7895@users.noreply.github.com>
2020-11-23 23:51:35 +0200

    Particel system

--- WillGame/Assets/Scripts/Level1Code.cs
+++ WillGame/Assets/Scripts/Level1Code.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Level1Code : MonoBehaviour
 {
     public GameObject parent;
+    public GameObject particle;
+
     GameObject g;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        particle.SetActive(false);
     }
 
     // Update is called once per frame

922113021f8d4b99712c2ca4f98e231fb1e268fa
brendan7895 <43366422+brendan7895@users.noreply.github.com>
2020-11-23 21:01:30 +0200

    Added Jellyfish

---
+++ WillGame/Assets/Scripts/PlaceJellyfish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceJellyfish : MonoBehaviour
+{
+    public GameObject jellyfish;
+    GameObject[] jellyfishArr = new GameObject[50];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < jellyfishArr.Length; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-100, 100), Random.Range(4 + 1, 22), Random.Range(-100, 100));
+
+            jellyfishArr[i] = Instantiate(jellyfish, pos, Quaternion.identity);
+            jellyfishArr[i].transform.Rotate(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
